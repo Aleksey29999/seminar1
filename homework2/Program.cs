a47e42687295620cfb2cf90573ade76649ede3c4
Aleksey <lexx29@yandex.ru>
2022-12-16 23:27:09 +0300

    задача 2

---
+++ homework2/Program.cs
@@ -0,0 +1,15 @@
+﻿// нахождение большего из двух чисел 
+Console.WriteLine("введите число a=");
+ int a = Convert.ToInt32(Console.ReadLine());
+ Console.WriteLine("введите число b=");
+ int b = Convert.ToInt32(Console.ReadLine());;
+if (a>b)
+{
+    Console.WriteLine("max="); 
+    System.Console.WriteLine(a); 
+}
+else
+{
+   Console.WriteLine("max="); 
+   System.Console.WriteLine(b);   
+}q
