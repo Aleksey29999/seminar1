af914578f4ac1729c156d2af0e03fa4373daecd4
Aleksey <lexx29@yandex.ru>
2022-12-16 23:47:57 +0300

    задача 4

---
+++ homework4/Program.cs
@@ -0,0 +1,33 @@
+﻿
+// нахождение большего из трех чисел 
+Console.WriteLine("введите число a=");
+ int a = Convert.ToInt32(Console.ReadLine());
+ Console.WriteLine("введите число b=");
+ int b = Convert.ToInt32(Console.ReadLine());
+ Console.WriteLine("введите число c=");
+ int c = Convert.ToInt32(Console.ReadLine());
+ 
+ if (a>b)
+{
+    if (a>c)
+    {
+    Console.WriteLine("max="); 
+    System.Console.WriteLine(a);
+    }
+    else
+    {
+   Console.WriteLine("max="); 
+   System.Console.WriteLine(c);   
+}
+}
+else
+if (b>c)
+    {
+    Console.WriteLine("max="); 
+    System.Console.WriteLine(b);
+    }
+else  
+{
+   Console.WriteLine("max="); 
+   System.Console.WriteLine(c);
+}
