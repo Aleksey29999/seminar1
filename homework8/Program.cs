a8056869b8ccabd8e1ca9a11263e3eb43a12221e
Aleksey <lexx29@yandex.ru>
2022-12-16 23:46:51 +0300

    задача 8

---
+++ homework8/Program.cs
@@ -0,0 +1,16 @@
+﻿// показывает все четные  от 1 до N
+
+Console.WriteLine("введите число");
+ int Number = Convert.ToInt32(Console.ReadLine());
+int Number1 = 1;
+while (Number1 <= Number)
+{if  (Number1 % 2 != 0)
+{
+ Number1++; 
+  }
+  else
+{
+System.Console.WriteLine(Number1);    
+Number1++;
+}
+ }
