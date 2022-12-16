0d82aa7cfa2d0b080d084c786578b97f18e94ca4
Aleksey <lexx29@yandex.ru>
2022-12-16 23:47:42 +0300

    задача 6

---
+++ homework6/Program.cs
@@ -0,0 +1,11 @@
+﻿//делится ли число без остатка 
+Console.WriteLine("введите число");
+ int a = Convert.ToInt32(Console.ReadLine());
+if  (a % 2 != 0)
+{
+System.Console.WriteLine("нет");
+    }
+else
+{
+System.Console.WriteLine("да");
+    }
